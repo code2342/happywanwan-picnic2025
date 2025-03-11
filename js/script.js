f65f0f4face2026302c1b471d284d16e1264a75b
DESKTOP-B4TP8CC\USER <kogimaruu@gmail.com>
2025-03-12 07:41:54 +0900

    スムーススクロール追加

--- js/script.js
+++ js/script.js
@@ -87,3 +87,82 @@ document.addEventListener("DOMContentLoaded", function () {
     observer.observe(card);
   });
 });
+
+jQuery("#js-drawer-icon").on("click", function (e) {
+  e.preventDefault();
+  jQuery("#js-drawer-icon").toggleClass("is-checked");
+  jQuery("#js-drawer-content").toggleClass("is-checked");
+});
+
+jQuery(".js-accordion").on("click", function (e) {
+  e.preventDefault();
+  if (jQuery(this).parent().hasClass("is-open")) {
+    jQuery(this).parent().removeClass("is-open");
+    jQuery(this).next().slideUp();
+  } else {
+    jQuery(this).parent().addClass("is-open");
+    jQuery(this).next().slideDown();
+  }
+});
+
+const swiper = new Swiper("#js-gallery-swiper", {
+  spaceBetween: 82,
+  // Optional parameters
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: "#js-gallery-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: "#js-gallery-prev",
+    prevEl: "#js-gallery-next",
+  },
+});
+
+jQuery(".js-modal-open").on("click", function (e) {
+  e.preventDefault();
+
+  jQuery("#js-about-modal")[0].showModal();
+});
+
+jQuery(".js-modal-close").on("click", function (e) {
+  e.preventDefault();
+
+  jQuery("#js-about-modal")[0].close();
+});
+
+jQuery('#js-drawer-content a[href^="#"]').on("click", function (e) {
+  jQuery("#js-drawer-icon").removeClass("is-checked");
+  jQuery("#js-drawer-content").removeClass("is-checked");
+});
+
+// スムーススクロール
+jQuery('a[href^="#"]').on("click", function (e) {
+  const speed = 1000;
+  const id = jQuery(this).attr("href");
+  const target = jQuery("#" === id ? "html" : id);
+  const position = jQuery(target).offset().top;
+
+  jQuery("html,body").animate(
+    {
+      scrollTop: position,
+    },
+    speed,
+    "swing" //swing or linear
+  );
+});
+
+jQuery(window).on("scroll", function () {
+  if (100 < jQuery(window).scrollTop()) {
+    jQuery("#js-pagetop").addClass("is-show");
+  } else {
+    jQuery("#js-pagetop").removeClass("is-show");
+  }
+});
+
+// =======================================
+// トコトコアニメーション
+// =======================================
